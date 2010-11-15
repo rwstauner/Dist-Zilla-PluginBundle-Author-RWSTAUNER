3b452506527e441205634fd1785617785b3a15e2
Randy Stauner <randy@magnificent-tears.com>
2010-11-15 08:09:33 -0700

    Commit initial PluginBundle modules
    * Dist::Zilla
    * Pod::Weaver

---
+++ lib/Dist/Zilla/PluginBundle/GopherRepellent.pm
@@ -0,0 +1,266 @@
+package Dist::Zilla::PluginBundle::GopherRepellent;
+# ABSTRACT: keep those pesky gophers out of your dists!
+
+use strict;
+use warnings;
+use Moose;
+use Moose::Autobox;
+use Dist::Zilla 2.100922; # TestRelease
+with 'Dist::Zilla::Role::PluginBundle::Easy';
+
+# TODO: use modules
+
+our $NAME = join('', '@', (__PACKAGE__ =~ /([^:]+)$/));
+
+# attributes
+
+has auto_prereqs => (
+	is      => 'ro',
+	isa     => 'Bool',
+	lazy    => 1,
+	default => sub {
+		exists $_[0]->payload->{auto_prereqs}
+		     ? $_[0]->payload->{auto_prereqs}
+			 : 1
+	}
+);
+
+has fake_release => (
+	is      => 'ro',
+	isa     => 'Bool',
+	lazy    => 1,
+	default => sub { $_[0]->payload->{fake_release} || $ENV{DZIL_FAKERELEASE} }
+);
+
+has is_task => (
+	is      => 'ro',
+	isa     => 'Bool',
+	lazy    => 1,
+	default => sub { $_[0]->payload->{is_task} }
+);
+
+has skip_prereqs => (
+	is      => 'ro',
+	isa     => 'Str',
+	lazy    => 1,
+	default => sub { $_[0]->payload->{skip_prereqs} || '' }
+);
+
+has weaver_config => (
+	is      => 'ro',
+	isa     => 'Str',
+	lazy    => 1,
+	default => sub { $_[0]->payload->{weaver_config} || $NAME }
+);
+
+sub configure {
+	my ($self) = @_;
+
+	$self->log_fatal("you must not specify both weaver_config and is_task")
+		if $self->is_task and $self->weaver_config ne $NAME;
+
+	$self->add_plugins(
+	
+	# provide version
+		'Git::DescribeVersion',
+
+	# gather and prune
+		qw(
+			GatherDir
+			PruneCruft
+			ManifestSkip
+		),
+
+	# munge files
+		'PkgVersion',
+		# 'Prepender',
+		( $self->is_task
+			?  'TaskWeaver'
+			: [ 'PodWeaver' => { config_plugin => $self->weaver_config } ]
+		),
+
+	# generated distribution files
+		qw(
+			License
+			Readme
+		),
+		[
+			GitFmtChanges => {
+				file_name  => 'Changes',
+				log_format => 'format:%h %s%n'
+			}
+		],
+
+	# metadata
+		[
+			Repository => { github_http => 0 }
+		],
+		( $self->auto_prereqs
+			? [ 'AutoPrereqs' => { $self->skip_prereqs ? (skip => $self->skip_prereqs) : () } ]
+			: ()
+		),
+		qw(
+			MinimumPerl
+			MetaConfig
+			MetaYAML
+			MetaJSON
+		),
+
+#		[
+#			MetaNoIndex => { 
+#				directory => [qw/t xt examples corpus/],
+#				'package' => [qw/DB/]
+#			} 
+#		],
+#		['MetaProvides::Package' => { meta_noindex => 1 } ], # AFTER MetaNoIndex
+
+		[
+			Prereqs => 'TestMoreWithSubtests' => {
+				-phase => 'test',
+				-type  => 'requires',
+				'Test::More' => '0.96'
+			}
+		],
+
+	# build system
+		qw(
+			ExtraTests
+			ExecDir
+			ShareDir
+			MakeMaker
+		),
+
+		#;[ModuleBuild]
+		#;[DualBuilders]
+		#;prefer = make
+
+	# generated t/ tests
+		[ CompileTests => { fake_home => 1 } ],
+
+	# generated xt/ tests
+		qw(
+			MetaTests
+			PodSyntaxTests
+			PodCoverageTests
+			PortabilityTests
+			KwaliteeTests
+			MinimumVersionTests
+		),
+
+	# manifest: must come after all generated files
+		'Manifest',
+
+	# before release
+			#CheckExtraTests
+		qw(
+			TestRelease
+			ConfirmRelease
+		),
+
+	# release
+    	( $self->fake_release ? 'FakeRelease' : 'UploadToCPAN' ),
+	);
+
+#	$self->add_bundle('@Git' => {
+#		tag_format => '%v',
+#		push_to    => [ qw(origin github) ],
+#	});
+
+}
+
+__PACKAGE__->meta->make_immutable;
+no Moose;
+1;
+
+=head1 SYNOPSIS
+
+	# dist.ini
+
+	[@GopherRepellent]
+
+=head1 DESCRIPTION
+
+This is a L<Dist::Zilla::PluginBundle> to help
+keep those pesky gophers away from your dists.
+
+It is roughly equivalent to:
+
+	[Git::DescribeVersion]  ; count commits from last tag to provide version
+
+	; choose files to include (dzil core [@Basic])
+	[GatherDir]             ; everything under top dir
+	[PruneCruft]            ; default stuff to skip
+	[ManifestSkip]          ; custom stuff to skip
+
+	; munge files
+	[PkgVersion]            ; inject $VERSION into modules
+
+	[PodWeaver]             ; munge POD in all modules
+	config_plugin = @GopherRepellent
+	; 'weaver_config' can be set to an alternate Bundle
+	; set 'is_task = 1' to use TaskWeaver instead
+
+	; generate files
+	[License]               ; generate distribution files (dzil core [@Basic])
+	[Readme]
+	[GitFmtChanges]         ; generate a Changes file from git log --oneline
+	file_name = Changes
+	log_format = format:%h %s%n
+
+	; metadata
+	[Repository]            ; determine git[hub] information
+	github_http = 0         ; let META.json show both url's
+
+	[AutoPrereqs]
+	; disable with 'auto_prereqs = 0'
+	[MinimumPerl]           ; automatically determine Perl version required
+
+	[MetaConfig]            ; include Dist::Zilla info in distmeta
+	[MetaYAML]              ; include META.yml (v1.4) (dzil core [@Basic])
+	[MetaJSON]              ; include META.json (v2) (more info than META.yml)
+
+	; build system
+	[ExtraTests]            ; build system (dzil core [@Basic])
+	[ExecDir]               ; include 'bin/*' as executables
+	[ShareDir]              ; include 'share/' for File::ShareDir
+	[MakeMaker]             ; create Makefile.PL
+
+	; generate t/ tests
+	[CompileTests]          ; make sure .pm files all compile
+	fake_home = 1           ; fakes $ENV{HOME} just in case
+
+	; generate xt/ tests
+	[MetaTests]             ; test META
+	[PodSyntaxTests]        ; test POD
+	[PodCoverageTests]      ; test documentation coverage
+	[PortabilityTests]      ; test portability (why? who doesn't use Linux?)
+	[KwaliteeTests]         ; CPANTS
+	[MinimumVersionTests]   ; test that the automatic plugin worked
+
+	[Manifest]              ; build MANIFEST file (dzil core [@Basic])
+	
+	; actions for releasing the distribution (dzil core [@Basic])
+	[TestRelease]           ; run tests before releasing
+	[ConfirmRelease]        ; are you sure?
+	[UploadToCPAN]
+	; set 'fake_release = 1' to use [FakeRelease] instead
+
+This Bundle was heavily influenced by the bundles of
+L<RJBS|Dist::Zilla::PluginBundle::RJBS> and
+L<DAGOLDEN|Dist::Zilla::PluginBundle::DAGOLDEN>.
+
+=head1 CAVEATS
+
+This bundle is essentially BeLike::RWSTAUNER.
+(And who would want to do that?)
+
+It is subject to change.
+
+I am still learning and exploring the possibilities.
+
+So this is mostly for my own use
+(and for people I work with who are less inclined to roll their own),
+but perhaps my choices and documentation will help others along the way
+(or encouarage someone to set me straight).
+
+=cut
---
+++ lib/Pod/Weaver/PluginBundle/GopherRepellent.pm
@@ -0,0 +1,105 @@
+package Pod::Weaver::PluginBundle::GopherRepellent;
+# ABSTRACT: keep those pesky gophers out of your POD!
+
+use strict;
+use warnings;
+
+use Pod::Weaver::Config::Assembler;
+sub _exp { Pod::Weaver::Config::Assembler->expand_package($_[0]) }
+
+# TODO: use modules
+
+our $NAME = join('', '@', (__PACKAGE__ =~ /([^:]+)$/));
+
+sub mvp_bundle_config {
+  my @plugins;
+  push @plugins, (
+    #[ "$NAME/WikiDoc",     _exp('-WikiDoc'), {} ],
+    [ "$NAME/CorePrep",    _exp('@CorePrep'), {} ],
+    [ "$NAME/Name",        _exp('Name'),      {} ],
+    [ "$NAME/Version",     _exp('Version'),   {} ],
+
+    [ "$NAME/Prelude",     _exp('Region'),  { region_name => 'prelude'     } ],
+    [ "$NAME/Synopsis",    _exp('Generic'), { header      => 'SYNOPSIS'    } ],
+    [ "$NAME/Description", _exp('Generic'), { header      => 'DESCRIPTION' } ],
+    [ "$NAME/Overview",    _exp('Generic'), { header      => 'OVERVIEW'    } ],
+
+    #[ "$NAME/Stability",   _exp('Generic'), { header      => 'STABILITY'   } ],
+  );
+
+  for my $plugin (
+    [ 'Attributes', _exp('Collect'), { command => 'attr'   } ],
+    [ 'Methods',    _exp('Collect'), { command => 'method' } ],
+    [ 'Functions',  _exp('Collect'), { command => 'func'   } ],
+  ) {
+    $plugin->[2]{header} = uc $plugin->[0];
+    push @plugins, $plugin;
+  }
+
+  push @plugins, (
+	# include Support section with various cpan links and github repo
+    [ "$NAME/Support",   _exp('Support'),
+		{
+			repository_content => '',
+			repository_link => 'both'
+		}
+	],
+
+    [ "$NAME/Leftovers", _exp('Leftovers'), {} ],
+    [ "$NAME/postlude",  _exp('Region'),    { region_name => 'postlude' } ],
+    [ "$NAME/Authors",   _exp('Authors'),   {} ],
+    [ "$NAME/Legal",     _exp('Legal'),     {} ],
+    [ "$NAME/List",      _exp('-Transformer'), { 'transformer' => 'List' } ],
+  );
+
+  return @plugins;
+}
+
+1;
+
+=for Pod::Coverage mvp_bundle_config
+
+=head1 DESCRIPTION
+
+Roughly equivalent to:
+
+	; like @Default {
+	[@CorePrep]
+
+	[Name]
+	[Version]
+
+	[Region  / prelude]
+
+	[Generic / SYNOPSIS]
+	[Generic / DESCRIPTION]
+	[Generic / OVERVIEW]
+
+	[Collect / ATTRIBUTES]
+	command = attr
+
+	[Collect / METHODS]
+	command = method
+
+	[Collect / FUNCTIONS]
+	command = func
+
+	[Leftovers]
+
+	[Region  / postlude]
+
+	; custom section
+	[Support]
+	repository_content =
+	repository_link = both
+
+	; finish @Default
+	[Authors]
+	[Legal]
+	; } end of @Default
+
+	; append customizations
+	[-Transformer]
+	transformer = List
+
+=cut
